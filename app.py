7fbc2a509587d90df32d395939fe93f04a5cee05
Aashikhatri1 <134989740+Aashikhatri1@users.noreply.github.com>
2024-01-04 20:34:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
+# from flask import Flask, request, jsonify
+# import search_products_pplx  # Import your existing code here
+# from flask_cors import CORS
+
+# app = Flask(__name__)
+# CORS(app)  # Enable CORS for all routes
+
+# @app.route('/process_request', methods=['POST'])
+# def process_request():
+#     # Extract data from the request
+#     data = request.json
+#     session_id = data.get('session_id')
+#     user_question = data.get('user_question')
+
+#     # Process the request using your existing functions
+#     filter_data = search_products_pplx.text_to_text_conversation(session_id, user_question)
+#     product_links = search_products_pplx.create_url(filter_data)
+
+#     # Return the response
+#     # return jsonify(product_links)
+#     return product_links
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 from flask import Flask, request, jsonify
 import search_products_pplx  # Import your existing code here
 from flask_cors import CORS
@@ -13,12 +38,12 @@ def process_request():
     user_question = data.get('user_question')
 
     # Process the request using your existing functions
-    filter_data = search_products_pplx.text_to_text_conversation(session_id, user_question)
-    product_links = search_products_pplx.create_url(filter_data)
+    filter_data, message = search_products_pplx.text_to_text_conversation(session_id, user_question)
+    product_link = search_products_pplx.create_url(filter_data, message)
 
     # Return the response
     # return jsonify(product_links)
-    return product_links
+    return product_link
 
 if __name__ == '__main__':
     app.run(debug=True)
